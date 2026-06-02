2e83cf8169d982b5524f642edc98d8b7f4ed77a1
chandhiramouli-k <145318725+chandhiramouli-k@users.noreply.github.com>
2026-06-02 12:36:01 +0530

    Delete src directory

--- src/sentiment_mapping.py
+++
@@ -1,7 +0,0 @@
-def emotion_to_sentiment(emotion):
-    if emotion in ['happy', 'surprise']:
-        return "POSITIVE"
-    elif emotion == 'neutral':
-        return "NEUTRAL"
-    else:
-        return "NEGATIVE"
